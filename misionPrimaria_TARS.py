e5e15179caa36bd5baa1e98faabe67f758fb0055
Paula Taibo <paulataibo15@gmail.com>
2021-01-31 21:19:08 +0100

    Mejora de la presentación de datos en TARS

--- misionPrimaria_TARS.py
+++ misionPrimaria_TARS.py
@@ -4,14 +4,30 @@ import time
 
 def main():
 	radio.init()
+	startTime = 0
 
 	while True:
 		packet = radio.rfm69.receive()
 		if packet is None:
-			print("No packet")
+			pass
 		else:
-			print(packet)
-		time.sleep(1)
+			dataLine = packet.decode()
+			fields = dataLine.split (' ')
+			timeStamp = float(fields[0])
+
+			if startTime == 0:
+				startTime = timeStamp
+
+			temperature = float(fields[1])
+			humedad = float(fields[2])
+			presion = float(fields[3])
+			altitud = float(fields[4])
+			print("\nTime: %0.2f s" % (timeStamp - startTime))
+			print("Temperature: %0.1f C" % temperature)
+			print("Humedad: %0.1f %%" % humedad)
+			print("Presión: %0.1f hPa" % presion)
+			print("Altitud: %0.2f meters" % altitud)
+
 
 	radio.close()
 
